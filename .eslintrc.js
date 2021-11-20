922496f81e81e936e0fa384416901ea47a9e5fb2
朱晓江 <xiaojiang.zhu@mihoyo.com>
2021-11-21 02:16:56 +0800

    husky & lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,24 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/vue3-recommended',
     'airbnb-base',
+    // 'prettier',
+    'plugin:prettier/recommended',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 13,
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
+    // 'vue/script-setup-uses-vars': 'error',
+    // 'no-use-before-define': 'off',
+    // '@typescript-eslint/no-use-before-define': 'off',
   },
-};
+  globals: {
+    defineProps: 'readonly',
+  },
+}
